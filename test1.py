5036e05e1c664a636d534d1fa67c7b345e8e848b
Erwin Vásquez <contactwitherwin@gmail.com>
2022-06-30 16:52:06 -0600

    Testing Streamlit
    Testing Streamlit

--- test1.py
+++ test1.py
@@ -26,10 +26,10 @@ if uploaded_file:
     option = st.selectbox(
         '¿What Algorithm do you want to use in the previously loaded data set?',
         ('None','linear regression', 'polynomial regression', 'Gaussian classifier','Decision tree classifier','neural networks'))
-    st.write('You selected:', option)
     parameters_of_x=["None"]
     parameters_of_y=["None"]
     if(option=='linear regression'):
+        st.markdown("### Linear Regression")
         for column_name in keys.columns:
             parameters_of_x.append(column_name)
             parameters_of_y.append(column_name)
@@ -37,6 +37,14 @@ if uploaded_file:
             '¿What attribute will be taken in X?',parameters_of_x)
         options_in_y = st.selectbox(
             '¿What attribute will be taken in Y?',parameters_of_y)
+        year_of_prediction = st.text_input('Year Of Prediction', 'Ex. 2023')
+        if (options_in_x!='None' and options_in_y!='None' and year_of_prediction!='Ex. 2023'):
+            st.markdown("### All done")
+            
+
+
+def linearRegression(options_in_x,options_in_y):
+    print()
         
         
 
