dc1d4b3904ff91c4215428be0f1c93465d3e03b9
Erwin Vásquez <contactwitherwin@gmail.com>
2022-06-30 16:01:33 -0600

    Testing Streamlit
    Testing Streamlit

--- test1.py
+++ test1.py
@@ -21,12 +21,23 @@ if uploaded_file:
     df = pd.read_csv(uploaded_file)
     st.markdown("### Dataset preview")
     st.write(df)
+    keys=df.head()
     #Se hace visible un selectBox con todos los algoritmos disponibles a realizar
     option = st.selectbox(
-    '¿What Algorithm do you want to use in the previously loaded data set?',
-    ('linear regression', 'polynomial regression', 'Gaussian classifier','Decision tree classifier','neural networks'))
+        '¿What Algorithm do you want to use in the previously loaded data set?',
+        ('None','linear regression', 'polynomial regression', 'Gaussian classifier','Decision tree classifier','neural networks'))
     st.write('You selected:', option)
+    parameters_of_x=["None"]
+    parameters_of_y=["None"]
     if(option=='linear regression'):
-        st.write('holaaaaaaaa')
+        for column_name in keys.columns:
+            parameters_of_x.append(column_name)
+            parameters_of_y.append(column_name)
+        options_in_x = st.selectbox(
+            '¿What attribute will be taken in X?',parameters_of_x)
+        options_in_y = st.selectbox(
+            '¿What attribute will be taken in Y?',parameters_of_y)
+        
+        
 
     
